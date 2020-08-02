021b18703fad2795c8dfb161d96ddb024c1e386c
小石头 <qi284025258@163.com>
2020-08-02 18:55:28 +0800

    更新 20200801 作业  使用相对路径导入模块

--- homework/homework0801/run.py
+++ homework/homework0801/run.py
@@ -5,6 +5,7 @@ from homework.homework0801 import test_case
 
 if __name__ == "__main__":
     suite = unittest.defaultTestLoader.loadTestsFromModule(test_case)
+    # 通过路径加载测试用例，无法识别相对路径
     # suite = unittest.defaultTestLoader.discover(r"./")
     runner = TestRunner(suite=suite, tester="小石头", desc="小石头的测试报告")
     runner.run()
